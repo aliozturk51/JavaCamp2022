781fd3fc5ee8e1b9de755782eee32c5d8f8ec617
Ali <Ali@DESKTOP-D61F8NV>
2022-09-28 16:18:59 +0300

    1. ve 2. Hafta

--- recapDemo1/src/recapDemo1/recapDemo1.java
+++ recapDemo1/src/recapDemo1/recapDemo1.java
@@ -3,23 +3,19 @@ package recapDemo1;
 public class recapDemo1 {
 
 	public static void main(String[] args) {
-		
-		int sayi1= 20;
-		int sayi2= 25;
-		int sayi3= 2;
-		
-		int enBuyuk= sayi1;
-		
-		
-		if (enBuyuk<sayi2) {
-			enBuyuk= sayi2;
-			
+
+		int sayi1 = 20;
+		int sayi2 = 25;
+		int sayi3 = 2;
+
+		int enBuyuk = sayi1;
+
+		if (enBuyuk < sayi2) {
+			enBuyuk = sayi2;
+
 		}
 		System.out.println("En büyük " + enBuyuk);
-		
-		
 
-		
 	}
 
 }
